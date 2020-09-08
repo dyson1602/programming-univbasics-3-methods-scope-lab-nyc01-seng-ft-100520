c4ca379d79ebac7ad5d9ac52cd330f431cb821c0
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-08 18:39:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,6 +5,20 @@ def mario
   puts phrase
 end
 
+
 def toadstool
   puts $status
 end
+
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+
+def all_phrases
+  mario
+  toadstool
+  link
+  
